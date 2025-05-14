d3d28fdf092226cffe4a5fcf731187838b9164e5
Umesh Rasanjana <153415158+shone26@users.noreply.github.com>
2025-05-14 20:46:28 +0530

    setup the backend

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const express = require('express'); 
+const cors = require('cors');
+const morgan = require('morgan');
+const documentRoutes = require('./routes/documentRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const { initializeDatabase } = require('./config/database');
+const { intializeIndex } = require('./services/indexService');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+//initialize the database
+initializeDatabase();
+
+
+// Initialize inverted index
+initializeIndex();
+
+// Routes
+app.use('/api/documents', documentRoutes);
+app.use('/api/search', searchRoutes);
+
+// Base route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Document Search API. Document Retrieval System API is running');
+});
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+
+module.exports = app;
